4442a50e3544bbe417a940876a78fd3473cb3ff8
Dominique Martins <dominique_martins@outlook.com>
2024-02-24 12:55:00 -0300

    UI

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI _scoreEndText;
+    [SerializeField] private TextMeshProUGUI _endGameTitle;
+    [SerializeField] private GameObject _endGamePanel;
+    [SerializeField] private GameObject _scoreFrame;
+    
+    void Awake()
+    {
+        GameManager.Instance.OnScoreUpdate += OnScoreUpdate;
+        GameManager.Instance.OnGameEnd += EndGame;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.Instance.OnScoreUpdate -= OnScoreUpdate;
+        GameManager.Instance.OnGameEnd -= EndGame;
+    }
+    
+    private void EndGame(bool isWin)
+    {
+        _endGameTitle.text = isWin ? "You Win" : "You Lose";
+        _scoreEndText.text = "Score: " + GameManager.Instance.TotalScore;
+        _endGamePanel.gameObject.SetActive(true);
+        _scoreFrame.gameObject.SetActive(false);
+    }
+    
+    private void OnScoreUpdate()
+    {
+        _scoreText.text = "Score: " + GameManager.Instance.TotalScore;
+    }
+}
